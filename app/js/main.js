e848c12609140a89c850b3c84925c578d7e09bba
Ches Chesy <chesy47@gmail.com>
2020-03-13 21:11:41 +0200

    Add category page

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,15 @@ $(function() {
     rating: 5,
     starWidth: "15px"
   }); 
+
+ $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 350,
+        from: 30,
+        to: 300,
+        prefix: "$"
+});
     
     
 });
