3e3ab80b3f295ae67ea18b4cdf6f3f66d10f25eb
Ches Chesy <chesy47@gmail.com>
2020-03-16 14:33:26 +0200

    Add Page_list

--- app/js/main.js
+++ app/js/main.js
@@ -14,5 +14,18 @@ $(function() {
         prefix: "$"
 });
     
+$('.icon-list2').on('click', function() {
+    $('.product__block').addClass('list');
+    $('.icon-list2').addClass('active');
+    $('i.fas.fa-th-large').removeClass('active');
+});
+    
+$('i.fas.fa-th-large').on('click', function() {
+    $('.product__block').removeClass('list');
+    $('i.fas.fa-th-large').addClass('active');
+    $('.icon-list2').removeClass('active');
+});
+    
+    
     
 });
