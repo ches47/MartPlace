9d4be9b1ff2fd012ee9884e25574bce024b72eb4
Ches Chesy <chesy47@gmail.com>
2020-03-31 12:17:28 +0300

    Maked all JS in site

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 $(function() {
- 
+    
   $(".rate-star").rateYo({
     rating: 5,
     starWidth: "15px"
@@ -26,6 +26,28 @@ $('i.fas.fa-th-large').on('click', function() {
     $('.icon-list2').removeClass('active');
 });
     
+$('.product-one__tabs .tab, settings__tabs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+        $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs, .settings__tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+    });   
+    
     
     
 });
+
+ const mixer = mixitup('.content-blocks');
+
+$('.weekly__inner').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<a href="#" class="weekly__title-chevron  weekly__title-right"><i class="fas fa-chevron-left"></i></a>', 
+    nextArrow: '<a href="#" class="weekly__title-chevron  weekly__title-left"><i class="fas fa-chevron-right"></i></a>', 
+});
+
+
+
+
