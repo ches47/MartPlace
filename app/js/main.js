7f176562101d2f8543ae118e8022600ae6b0cb2b
Ches Chesy <chesy47@gmail.com>
2020-04-06 18:23:51 +0300

    Header Adaptive all done

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
+//On SCROLL
+let upButton = document.querySelector('.up-button');
+
+window.onscroll = function () {
+  if(window.pageYOffset > 800) {
+    upButton.classList.add('shown');
+  } else {
+    upButton.classList.remove('shown');  
+  }  
+};
+
+upButton.onclick = function () {
+  window.scrollTo(0, 0);
+};
+
+
+
+
+
 $(function() {
     
   $(".rate-star").rateYo({
